79a3b30af9f6faa08658f8041169bf9c61ee9038
Dihuu <62663759+mahirlabibdihan@users.noreply.github.com>
2020-09-15 15:05:35 +0600

    Update Decimal_Binary_Octal_Converter_L.c

--- Converter/Decimal_Binary_Octal_Converter_L.c
+++ Converter/Decimal_Binary_Octal_Converter_L.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
 
 void gotoxy(int x, int y)
 {COORD c;c.X=x;c.Y=y;SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);}
@@ -31,32 +32,40 @@ long long fracconvert(double n,int from,int to)
 
 int main()
 {
-	system("mode con: lines=20 cols=58");
+	system("mode con: lines=22 cols=58");
 
-    int end=1;
-    system("cls");
+    
+    while(1)
+	{
+	system("cls");
+    char c;
 	int from,to; double n;
 	long long a,b;
     
     gotoxy(10,1);printf("DECIMAL-BINARY-OCTAL CONVERTER");
-	gotoxy(1,3);printf("(2->Binary , 8->Octal , 10->Decimal) \n Base of the number to be converted -> ");
-	scanf("%d",&from);
+    gotoxy(8,2);printf("----------------------------------");
+	gotoxy(1,4);printf("(2->Binary , 8->Octal , 10->Decimal) \n Base of the number to be converted -> ");
+    gotoxy(39,6);printf("----");
+	gotoxy(40,5);scanf("%d",&from);
 
-	gotoxy(1,7);  printf("Enter the number : ");
-	gotoxy(20,5); printf(" _________________________________");
-	gotoxy(20,6); printf("|                                 |");
-    gotoxy(20,7); printf("|                                 |");
-    gotoxy(20,8); printf("|_________________________________|");
-	gotoxy(22,7); scanf("%lf",&n);
+	gotoxy(1,9);  printf("Enter the number : ");
+	gotoxy(20,7); printf(" _________________________________");
+	gotoxy(20,8); printf("|                                 |");
+    gotoxy(20,9); printf("|                                 |");
+    gotoxy(20,10);printf("|_________________________________|");
+	gotoxy(22,9); scanf("%lf",&n);
 
-	gotoxy(1,10);printf("Base of the number converted to -> ");
-	scanf("%d",&to);
+	gotoxy(1,12);printf("Base of the number converted to -> ");
+	gotoxy(36,13);printf("----");
+	gotoxy(37,12);scanf("%d",&to);
 
-	gotoxy(1,13); printf("Converted number : ");
-	gotoxy(20,11);printf(" _________________________________");
-	gotoxy(20,12);printf("|                                 |");
-    gotoxy(20,13);printf("|                                 |");
-    gotoxy(20,14);printf("|_________________________________|");
-	gotoxy(22,13);printf("%20llu.%lld",intconvert(n,from,to),fracconvert(n-(long long)n,from,to));
-	gotoxy(20,15);printf("\n");
+	gotoxy(1,16); printf("Converted number : ");
+	gotoxy(20,14);printf(" _________________________________");
+	gotoxy(20,15);printf("|                                 |");
+    gotoxy(20,16);printf("|                                 |");
+    gotoxy(20,17);printf("|_________________________________|");
+	gotoxy(22,16);printf("%20llu.%lld",intconvert(n,from,to),fracconvert(n-(long long)n,from,to));
+	gotoxy(1,19);printf("Press x to exit.... "); c=getch();
+	if(c=='x') break;
+    }
 }
