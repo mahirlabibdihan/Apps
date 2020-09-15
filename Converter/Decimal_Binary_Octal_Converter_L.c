c3e6f06e4a7b04945b2647b67eac6de182a7fd25
Dihuu <62663759+mahirlabibdihan@users.noreply.github.com>
2020-09-15 15:06:19 +0600

    Update Decimal_Binary_Octal_Converter_L.c

--- Converter/Decimal_Binary_Octal_Converter_L.c
+++ Converter/Decimal_Binary_Octal_Converter_L.c
@@ -21,7 +21,7 @@ unsigned long long intconvert(unsigned long long n,int from,int to)
 long long fracconvert(double n,int from,int to)
 {
 	 double j,d=n;   
-     long long b; 
+         long long b; 
 	 for(j=1,b=0;d!=0&j<12;j++) 
 		{   
 			b=(b*from)+(long long)d;
@@ -38,21 +38,21 @@ int main()
     while(1)
 	{
 	system("cls");
-    char c;
+        char c;
 	int from,to; double n;
 	long long a,b;
     
-    gotoxy(10,1);printf("DECIMAL-BINARY-OCTAL CONVERTER");
-    gotoxy(8,2);printf("----------------------------------");
+        gotoxy(10,1);printf("DECIMAL-BINARY-OCTAL CONVERTER");
+        gotoxy(8,2);printf("----------------------------------");
 	gotoxy(1,4);printf("(2->Binary , 8->Octal , 10->Decimal) \n Base of the number to be converted -> ");
-    gotoxy(39,6);printf("----");
+        gotoxy(39,6);printf("----");
 	gotoxy(40,5);scanf("%d",&from);
 
 	gotoxy(1,9);  printf("Enter the number : ");
 	gotoxy(20,7); printf(" _________________________________");
 	gotoxy(20,8); printf("|                                 |");
-    gotoxy(20,9); printf("|                                 |");
-    gotoxy(20,10);printf("|_________________________________|");
+        gotoxy(20,9); printf("|                                 |");
+        gotoxy(20,10);printf("|_________________________________|");
 	gotoxy(22,9); scanf("%lf",&n);
 
 	gotoxy(1,12);printf("Base of the number converted to -> ");
@@ -62,8 +62,8 @@ int main()
 	gotoxy(1,16); printf("Converted number : ");
 	gotoxy(20,14);printf(" _________________________________");
 	gotoxy(20,15);printf("|                                 |");
-    gotoxy(20,16);printf("|                                 |");
-    gotoxy(20,17);printf("|_________________________________|");
+        gotoxy(20,16);printf("|                                 |");
+        gotoxy(20,17);printf("|_________________________________|");
 	gotoxy(22,16);printf("%20llu.%lld",intconvert(n,from,to),fracconvert(n-(long long)n,from,to));
 	gotoxy(1,19);printf("Press x to exit.... "); c=getch();
 	if(c=='x') break;
